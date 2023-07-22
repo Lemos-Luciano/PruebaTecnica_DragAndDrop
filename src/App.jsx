6ef6b5268fb47d07984674e592410daaffa03f8f
Luciano Lemos <luciano.platzy@gmail.com>
2023-07-22 01:09:06 +0100

    Agregar función handleDragDrop y comentarios explicativos

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd"
 
@@ -10,19 +11,55 @@ const initialData = [
 
 
 function App() {
+  const [items, setItems] = useState(initialData)
 
-  const items = initialData
+  // const items = initialData
+
+  const handleDragDrop = (results) => {
+    console.log(results);
+    const {source, destination, type} = results;
+
+    // si arrastramos a un destino no valido, no hace nada
+    if (!destination) return;
+
+    // Si colocamos el item en el mismo sitio, no hace nada
+    if (
+      source.droppableId === destination.droppableId && 
+      source.index === destination.index
+      ) 
+      return;
+
+    if (type === 'group') {
+      // ["item1", "item2", "item3"]
+      const reorderedStores = [...items];
+
+      const sourceIndex = source.index
+      const destinationIndex = destination.index
+
+      // creamos un nuevo array con el item removido, indicamos que queremos quitar un elemento
+      // ["item2", "item3"]
+      const [removeItem] = reorderedStores.splice(sourceIndex, 1);
+
+      // Agrega en el index indicado el elemento elminidado. Se quita 0 y se agrega removeItem
+      // ["item2", "item3", "item1"]
+      reorderedStores.splice(destinationIndex, 0, removeItem)
+
+      return setItems(reorderedStores);
+
+    }
+  }
 
 
   return (
     <>
     <h1 className='text-lg font-bold'>Drag and Drop</h1>
-    <DragDropContext onDragEnd={() => {console.log("drag drog event ocurred");}}>
+    <DragDropContext 
+      onDragEnd={handleDragDrop}>
       {/* Aclaramos el id del droppable y el grupo */}
       <Droppable droppableId='ROOT' type='group'>
         {(provided) => (
           // marca el dip dropeable
-          <div {...provided.droppableProps} ref={provided.innerRef} className='bg-cyan-600 p-2'>
+          <div {...provided.droppableProps} ref={provided.innerRef} className='bg-cyan-600 p-2 rounded-2xl'>
             {
               items.map((item, index) => 
                 // marca los items dragrables
