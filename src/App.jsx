7c36ef2dd4ea1c439ef1e9b42db1c6901efcbd17
Luciano Lemos <luciano.platzy@gmail.com>
2023-07-22 13:37:32 +0100

    Agregado otro Droppable y habilitada la funcionalidad de arrastrar y soltar entre ellos

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import './App.css'
 import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd"
 
 
-const initialData = [
+const initialData = {
+  "ROOT" : [
   { id: "01", name: "item1" },
   { id: "02", name: "item2" },
   { id: "03", name: "item3" },
-];
+],
+ "cabezera" : [
+  { id: "04", name: "Cabezera1" },
+]
+}
 
 
 function App() {
   const [items, setItems] = useState(initialData)
 
-  // const items = initialData
 
   const handleDragDrop = (results) => {
     console.log(results);
@@ -31,21 +35,26 @@ function App() {
 
     if (type === 'group') {
       // ["item1", "item2", "item3"]
-      const reorderedStores = [...items];
+      const reorderedStores = {...items};
 
       const sourceIndex = source.index
       const destinationIndex = destination.index
 
+      const sourceDroppableID = source.droppableId
+      const destinationDroppableID = destination.droppableId
+
+
+      // if (sourceDroppableID === destinationDroppableID) 
+
       // creamos un nuevo array con el item removido, indicamos que queremos quitar un elemento
       // ["item2", "item3"]
-      const [removeItem] = reorderedStores.splice(sourceIndex, 1);
+      const [removeItem] = reorderedStores[sourceDroppableID].splice(sourceIndex, 1);
 
       // Agrega en el index indicado el elemento elminidado. Se quita 0 y se agrega removeItem
       // ["item2", "item3", "item1"]
-      reorderedStores.splice(destinationIndex, 0, removeItem)
+      reorderedStores[destinationDroppableID].splice(destinationIndex, 0, removeItem)
 
       return setItems(reorderedStores);
-
     }
   }
 
@@ -61,7 +70,30 @@ function App() {
           // marca el dip dropeable
           <div {...provided.droppableProps} ref={provided.innerRef} className='bg-cyan-600 p-2 rounded-2xl'>
             {
-              items.map((item, index) => 
+              items["ROOT"].map((item, index) => 
+                // marca los items dragrables
+                <Draggable draggableId={item.id} key={item.id} index={index} >
+                  {(provided) => (
+                    <div {...provided.dragHandleProps} {...provided.draggableProps} ref={provided.innerRef}>
+                      <h3 type="button" id={item.id} className='my-1 mx-32 border border-green-500 bg-green-500 text-white rounded-md hover:bg-green-600'>{item.name}</h3>
+                    </div>
+                  )}
+                </Draggable>)
+            }
+            {/* agrega un placeholder cuando arrastramos un elemento para que el contenedor no se reduzca de tamaño */}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+
+      <br />
+
+      <Droppable droppableId='cabezera' type='group'>
+        {(provided) => (
+          // marca el dip dropeable
+          <div {...provided.droppableProps} ref={provided.innerRef} className='bg-cyan-600 p-2 rounded-2xl'>
+            {
+              items["cabezera"].map((item, index) => 
                 // marca los items dragrables
                 <Draggable draggableId={item.id} key={item.id} index={index} >
                   {(provided) => (
